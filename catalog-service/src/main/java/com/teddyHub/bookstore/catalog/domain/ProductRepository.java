1cb7ec27c652b987d50a6b7236c7938f4896462a
teddy <tedsaasfaha@gmail.com>
2025-04-16 10:30:05 +0300

    create ProductRepository interface file: which extends JpaRepository

--- catalog-service/src/main/java/com/teddyHub/bookstore/catalog/domain/ProductRepository.java
+++ catalog-service/src/main/java/com/teddyHub/bookstore/catalog/domain/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.teddyHub.bookstore.catalog.domain;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
