a43241d611ca655abb99082f28d8d49fa8151511
teddy <tedsaasfaha@gmail.com>
2025-04-17 14:49:09 +0300

    create OrderNotFoundException exception file

--- order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderNotFoundException.java
+++ order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderNotFoundException.java
@@ -1,7 +1,11 @@
 package com.teddyHub.bookstore.orders.domain;
 
 public class OrderNotFoundException extends RuntimeException {
-  public OrderNotFoundException(String message) {
-    super(message);
-  }
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public static OrderNotFoundException forOrderNumber (String orderNumber) {
+        return new OrderNotFoundException("Order with number " + orderNumber + " not found");
+    }
 }
