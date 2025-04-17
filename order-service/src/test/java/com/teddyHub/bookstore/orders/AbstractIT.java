add9c96265e4c25ec2fe42709d3770f63e1ef5c2
teddy <tedsaasfaha@gmail.com>
2025-04-17 14:57:56 +0300

    create AbstractIT file: a generic test file configuration

--- order-service/src/test/java/com/teddyHub/bookstore/orders/AbstractIT.java
+++ order-service/src/test/java/com/teddyHub/bookstore/orders/AbstractIT.java
@@ -1,4 +1,20 @@
 package com.teddyHub.bookstore.orders;
 
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestContainersConfiguration.class)
 public class AbstractIT {
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 }
