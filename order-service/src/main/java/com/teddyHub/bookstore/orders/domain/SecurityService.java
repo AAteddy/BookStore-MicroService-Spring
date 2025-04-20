3ab11c0a2e7db36a1e7d9ae1ebdd0caf5b7775b5
teddy <tedsaasfaha@gmail.com>
2025-04-20 13:30:30 +0300

    create SecurityService class that holds getLoginUserName method which simply mocks to return a loggedin user

--- order-service/src/main/java/com/teddyHub/bookstore/orders/domain/SecurityService.java
+++ order-service/src/main/java/com/teddyHub/bookstore/orders/domain/SecurityService.java
@@ -1,4 +1,11 @@
 package com.teddyHub.bookstore.orders.domain;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SecurityService {
+
+    public String getLoginUserName() {
+        return "user";
+    }
 }
