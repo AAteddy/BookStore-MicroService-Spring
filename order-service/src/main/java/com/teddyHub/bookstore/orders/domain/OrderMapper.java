9a07b364ec79b95c3cd8429680e56dc2038ba5fa
teddy <tedsaasfaha@gmail.com>
2025-04-20 13:27:15 +0300

    create OrderMapper class: holds convertToEntity static method that maps to OrderEntity

--- order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderMapper.java
+++ order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderMapper.java
@@ -1,4 +1,33 @@
 package com.teddyHub.bookstore.orders.domain;
 
+import com.teddyHub.bookstore.orders.domain.models.CreateOrderRequest;
+// import com.teddyHub.bookstore.orders.domain.models.OrderDTO;
+import com.teddyHub.bookstore.orders.domain.models.OrderItem;
+import com.teddyHub.bookstore.orders.domain.models.OrderStatus;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+// import java.util.stream.Collectors;
+
 public class OrderMapper {
+
+    static OrderEntity convertToEntity(CreateOrderRequest request) {
+        OrderEntity newOrder = new OrderEntity();
+        newOrder.setOrderNumber(UUID.randomUUID().toString());
+        newOrder.setStatus(OrderStatus.NEW);
+        newOrder.setCustomer(request.customer());
+        newOrder.setDeliveryAddress(request.deliveryAddress());
+        Set<OrderItemEntity> orderItems = new HashSet<>();
+        for (OrderItem item : request.items()) {
+            OrderItemEntity orderItem = new OrderItemEntity();
+            orderItem.setCode(item.code());
+            orderItem.setName(item.name());
+            orderItem.setPrice(item.price());
+            orderItem.setQuantity(item.quantity());
+            orderItem.setOrder(newOrder);
+            orderItems.add(orderItem);
+        }
+        newOrder.setItems(orderItems);
+        return newOrder;
+    }
 }
