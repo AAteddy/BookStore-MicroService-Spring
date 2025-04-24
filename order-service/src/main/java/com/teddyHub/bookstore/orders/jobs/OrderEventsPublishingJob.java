e832014de585551ac4eb3b4a62999c7d4e6c1c37
teddy <tedsaasfaha@gmail.com>
2025-04-24 16:39:12 +0300

    create OrderEventsPublishingJob: handles a Scheduled Job to publish order events to RabbitMQ

--- order-service/src/main/java/com/teddyHub/bookstore/orders/jobs/OrderEventsPublishingJob.java
+++ order-service/src/main/java/com/teddyHub/bookstore/orders/jobs/OrderEventsPublishingJob.java
@@ -1,4 +1,27 @@
 package com.teddyHub.bookstore.orders.jobs;
 
+
+import com.teddyHub.bookstore.orders.domain.OrderEventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
 public class OrderEventsPublishingJob {
+    private static final Logger log = LoggerFactory.getLogger(OrderEventsPublishingJob.class);
+
+    private final OrderEventService orderEventService;
+
+    public OrderEventsPublishingJob(OrderEventService orderEventService) {
+        this.orderEventService = orderEventService;
+    }
+
+    @Scheduled(cron = "${orders.publish-order-events-job-cron}")
+    public void publishOrderEvents() {
+        log.info("Publishing Order Events at {}", Instant.now());
+        orderEventService.publishOrderEvents();
+    }
 }
