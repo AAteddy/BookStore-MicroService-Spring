a700bdb2f7619ece2129f9778ba6bee5c7fe1ef1
teddy <tedsaasfaha@gmail.com>
2025-04-24 16:04:09 +0300

    create OrderEventRepository: order event repository by extending JpaRepository

--- order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderEventRepository.java
+++ order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderEventRepository.java
@@ -1,4 +1,8 @@
 package com.teddyHub.bookstore.orders.domain;
 
-public interface OrderEventRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderEventRepository extends JpaRepository<OrderEventEntity, Long> {
 }
