dcfe11d384183a751b3794b82b5890b90e8b06d5
teddy <tedsaasfaha@gmail.com>
2025-04-24 16:52:35 +0300

    update OrderService: implementing any order to be saved to an order event with order event service

--- order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderService.java
+++ order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderService.java
@@ -3,6 +3,7 @@ package com.teddyHub.bookstore.orders.domain;
 
 import com.teddyHub.bookstore.orders.domain.models.CreateOrderRequest;
 import com.teddyHub.bookstore.orders.domain.models.CreateOrderResponse;
+import com.teddyHub.bookstore.orders.domain.models.OrderCreatedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,15 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderValidator orderValidator;
+    private final OrderEventService orderEventService;
 
-    public OrderService(OrderRepository orderRepository, OrderValidator orderValidator) {
+    public OrderService(
+            OrderRepository orderRepository,
+            OrderValidator orderValidator,
+            OrderEventService orderEventService) {
         this.orderRepository = orderRepository;
         this.orderValidator = orderValidator;
+        this.orderEventService = orderEventService;
     }
 
     public CreateOrderResponse createOrder(
@@ -29,6 +35,8 @@ public class OrderService {
         newOrder.setUserName(userName);
         OrderEntity savedOrder = this.orderRepository.save(newOrder);
         log.info("Created Order with orderNumber={}", savedOrder.getOrderNumber());
+        OrderCreatedEvent orderCreatedEvent = OrderEventMapper.buildOrderCreatedEvent(savedOrder);
+        orderEventService.save(orderCreatedEvent);
         return new CreateOrderResponse(savedOrder.getOrderNumber());
     }
 }
