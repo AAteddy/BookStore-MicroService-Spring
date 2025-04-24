c3acb09bb9802ccb2fc9470b09f4f02dcbe89dd7
teddy <tedsaasfaha@gmail.com>
2025-04-24 16:42:47 +0300

    create OrderEventPublisher: responsible for sending/publishing order events to rabbitMQ

--- order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderEventPublisher.java
+++ order-service/src/main/java/com/teddyHub/bookstore/orders/domain/OrderEventPublisher.java
@@ -1,4 +1,25 @@
 package com.teddyHub.bookstore.orders.domain;
 
+import com.teddyHub.bookstore.orders.ApplicationProperties;
+import com.teddyHub.bookstore.orders.domain.models.OrderCreatedEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderEventPublisher {
+    private final RabbitTemplate rabbitTemplate;
+    private final ApplicationProperties properties;
+
+    OrderEventPublisher(RabbitTemplate rabbitTemplate, ApplicationProperties properties) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.properties = properties;
+    }
+
+    public void publish(OrderCreatedEvent event) {
+        this.send(properties.newOrdersQueue(), event);
+    }
+
+    private void send(String routingKey, Object payload) {
+        rabbitTemplate.convertAndSend(properties.orderEventsExchange(), routingKey, payload);
+    }
 }
