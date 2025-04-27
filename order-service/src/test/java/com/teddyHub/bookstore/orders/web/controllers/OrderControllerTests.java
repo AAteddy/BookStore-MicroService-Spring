aa73f4cda04659004e6b3f1b44422de5b078d47a
teddy <tedsaasfaha@gmail.com>
2025-04-27 18:57:13 +0300

    update OrderControllerTests: add Nested test classes GetOrdersTests & GetOrderByOrderNumberTests

--- order-service/src/test/java/com/teddyHub/bookstore/orders/web/controllers/OrderControllerTests.java
+++ order-service/src/test/java/com/teddyHub/bookstore/orders/web/controllers/OrderControllerTests.java
@@ -1,17 +1,25 @@
 package com.teddyHub.bookstore.orders.web.controllers;
 
 import com.teddyHub.bookstore.orders.AbstractIT;
+import com.teddyHub.bookstore.orders.domain.models.OrderSummary;
 import com.teddyHub.bookstore.orders.testdata.TestDataFactory;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
+@Sql("/test-orders.sql")
 class OrderControllerTests extends AbstractIT {
 
     @Nested
@@ -64,6 +72,39 @@ class OrderControllerTests extends AbstractIT {
                     .post("/api/orders")
                     .then()
                     .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
+    }
+
+    @Nested
+    @WithMockUser(username = "siva")
+    class GetOrdersTests {
+        @Test
+        void shouldGetOrdersSuccessfully() {
+            List<OrderSummary> orderSummaries = given().when()
+                    .get("/api/orders")
+                    .then()
+                    .statusCode(200)
+                    .extract()
+                    .body()
+                    .as(new TypeRef<>() {});
+
+            assertThat(orderSummaries).hasSize(2);
+        }
+    }
+
+    @Nested
+    @WithMockUser(username = "siva")
+    class GetOrderByOrderNumberTests {
+        String orderNumber = "order-123";
+
+        @Test
+        void shouldGetOrderSuccessfully() {
+            given().when()
+                    .get("/api/orders/{orderNumber}", orderNumber)
+                    .then()
+                    .statusCode(200)
+                    .body("orderNumber", is(orderNumber))
+                    .body("items.size()", is(2));
         }
     }
 }
